86354476871d5616d83b5121e15e4824b06f7dc5
codelord27-hub <swayamamazing@gmail.com>
2026-08-25 00:05:40 +0530

    Time: 6 ms (55.91%), Space: 13.6 MB (84.04%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans = "";
+        for (int i = 0; i<s.length(); i++){
+            char ch = s[i]; //similar to what we take intvale =arr[i];
+            if (ans.empty()){
+                ans.push_back(ch);
+            }
+            else if (ch != ans.back()){
+                ans.push_back(ch);
+            }
+            else{
+                ans.pop_back();
+            }
+            //return ans;
+        }
+        return ans;
+    }
+};
