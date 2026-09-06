1bac0ef6711aab50152f2b76f5a0d64847fcbf35
codelord27-hub <swayamamazing@gmail.com>
2026-09-06 13:50:14 +0530

    Time: 0 ms (100%), Space: 12.7 MB (49.31%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -4,7 +4,7 @@ public:
         int n = nums.size();
         if (n == 0) return 0;
         if (n == 1) return 0;
-        if (nums[0] > nums[1]) return 0;
+        if (nums[0] >= nums[1]) return 0;
         if (nums[n-1] > nums[n-2]) return n-1;
         int left = 1;
         int right = n-1;
