19f656ea1702e9d4e830faa04ebf40341ec8bcf9
codelord27-hub <swayamamazing@gmail.com>
2026-09-12 02:57:31 +0530

    Time: 8 ms (56.27%), Space: 30.9 MB (66.41%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,31 +1,24 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        int count = 0;
-        set <int> st;
+        set<int> st;
 
-        //if (digits != 2)
-        for (int i = 0; i < digits.size(); i++) {
-            if (digits[i] == 0)
-                continue; // no leading zero
+        for (int i = 0; i<digits.size(); i++){
+            if(digits[i] == 0) continue;
 
-            for (int j = 0; j < digits.size(); j++) {
-                if (j == i)
-                    continue;
+            for (int j = 0; j<digits.size(); j++){
+                if(j == i) continue;
 
-                for (int k = 0; k < digits.size(); k++) {
-                    if (k == i ||  k == j) continue;
-                    if (digits[k] % 2 != 0) continue;
+                for (int k = 0; k<digits.size(); k++){
+                    if(k == i || k == j) continue;
+                    if (digits[k] %2 != 0)continue;
 
                     int num = digits[i] * 100 + digits[j] * 10 + digits[k];
+
                     st.insert(num);
-                    //count++;
                 }
             }
         }
-
-        
-
         return st.size();
     }
 };
